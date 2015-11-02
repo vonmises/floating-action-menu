99d995bc1151b0749a49d46e666a8b2e3e7bd9c5
vonmises <vonmises@users.noreply.github.com>
2015-11-02 18:13:59 +0300

    first commit - added buttons (not linked to activities)

---
+++ app/src/main/java/com/andrew/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.getbase.floatingactionbutton.sample;
+
+import com.andrew.test.R;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
