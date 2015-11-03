105467fd5138e9b1ecb978023d2b4ea0105efc85
vonmises <vonmises@users.noreply.github.com>
2015-11-03 12:40:21 +0300

    added methods to create a bar dataset and x-axis labels

--- app/src/main/java/com/andrew/test/ChartActivity.java
+++ app/src/main/java/com/andrew/test/ChartActivity.java
@@ -5,13 +5,49 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
 public class ChartActivity extends AppCompatActivity {
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+/*
+        BarDataSet barSet = createBarDataSet();
+        ArrayList<String> labels = createXAxisLabels();
+        barChart = (BarChart) findViewById(R.id.barChart);*/
     }
+
+    public BarDataSet createBarDataSet(){
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        return new BarDataSet(entries, "# of Phone Calls");
+    }
+
+    public ArrayList<String> createXAxisLabels(){
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        return labels;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
