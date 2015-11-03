fd67819063180f84bad1affba00f7a4bc2b1ea2c
vonmises <vonmises@users.noreply.github.com>
2015-11-03 10:42:29 +0300

    removed toggle ability for hidden buttons

--- app/src/main/java/com/andrew/test/MainActivity.java
+++ app/src/main/java/com/andrew/test/MainActivity.java
@@ -12,21 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionsMenu floating_menu;
+    //FloatingActionsMenu floating_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floating_menu = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        //floating_menu = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
 
         findViewById(R.id.data).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DataActivity.class);
                 startActivity(intent);
-                floating_menu.toggle();
+                //floating_menu.toggle();
             }
         });
 
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ChartActivity.class);
                 startActivity(intent);
-                floating_menu.toggle();
+                //floating_menu.toggle();
             }
         });
 
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SummaryActivity.class);
                 startActivity(intent);
-                floating_menu.toggle();
+                //floating_menu.toggle();
             }
         });
 
